e8d5afce72c556ffd6df211f14ed6c1ac361d38e
robinson <robinpaulista85@gmail.com>
2024-05-16 02:40:50 -0300

    adicionando resolvers no index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import { ApolloServer, gql } from 'apollo-server';
 
-
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      hello: String
+      hello: String,
+      hi: String
     }
   `,
-  });
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'Hello again!';
+      },
+      hi: () => {
+        return 'Hi!';
+      },
+    },
+  },
+});
 
 server.listen(4003).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
